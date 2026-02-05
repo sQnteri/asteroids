0782a022a5fbb1bc58a79f3f8e14d8747019534f
sQnteri <haapaniemisanteri@gmail.com>
2026-02-05 22:18:20 +0200

    added dt and circleshape class

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from constants import *
 from logger import log_state
 
 def main():
-    pygame.init()
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
@@ -15,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
